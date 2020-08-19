ae9e9035be908f88b112c86489cd3aaed4581680
Krishna M <mvrkrishna@gmail.com>
2020-08-20 01:11:21 +0530

    First commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=mvrkrishna
+export POSTGRES_PASSWORD=ishaanarkaan
+export POSTGRES_HOST=database-module4.czjwdnvtw3qh.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=krishna-uda-module-4 
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
